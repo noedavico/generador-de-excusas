2a96a338d696d5677d931ea2fb1de7c757d2dcaf
noedavico <noedavico@hotmail.com>
2022-11-15 23:51:34 +0000

    actualizacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload;
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let rand_who = Math.floor(Math.random() * who.length);
+let rand_action = Math.floor(Math.random() * action.length);
+let rand_what = Math.floor(Math.random() * what.length);
+let rand_when = Math.floor(Math.random() * when.length);
+
+const combined = [].concat(rand_who, rand_action, rand_what, rand_when);
+console.log(combined);
