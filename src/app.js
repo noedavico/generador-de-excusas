af60a864e6d78a8f283727f16a02d8a7380d0caa
noedavico <noedavico@hotmail.com>
2022-11-17 09:53:54 +0000

    retoque

--- src/app.js
+++ src/app.js
@@ -1,25 +1,43 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload;
 {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
+  const excuse = document.querySelector("#excuse");
+  let who = [
+    "El perro",
+    "Mi abuela",
+    "El Gato",
+    "Mi vecino",
+    "Mi hijo",
+    "El cerdo"
+  ];
+  let action = ["comió", "destruyo", "meó", "aplastó", "rompió", "escondio"];
+  let what = [
+    "mi tarea",
+    "las llaves",
+    "el auto",
+    "el celular",
+    "el control",
+    "la planta",
+    "el libro"
+  ];
   let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando jugaba",
+    "durante mi almuerzo",
+    "mientras estaba orando",
+    "cuando meditaba",
+    "mientras me duchaba",
+    "cuando dormia",
+    "mientras estudiaba",
+    "cuando estaba trabajando"
   ];
   let rand_who = Math.floor(Math.random() * who.length);
   let rand_action = Math.floor(Math.random() * action.length);
   let rand_what = Math.floor(Math.random() * what.length);
   let rand_when = Math.floor(Math.random() * when.length);
 
-  const combined = [].concat(rand_who, rand_action, rand_what, rand_when);
+  excuse.innerHTML = `${who[rand_who]}
+  ${action[rand_action]}
+    ${what[rand_what]}
+      ${when[rand_when]}`;
 }
